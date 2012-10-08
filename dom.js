5c8b1222243f9bbd553969bd3c55aa64f1d63197
Ishaan Gulrajani <igul222@gmail.com>
2012-10-07 20:27:43 -0400

    Refactor includes; add html2canvas file (not yet used anywhere though)

--- dom.js
+++ dom.js
@@ -1,125 +1,99 @@
 // since this is supposed to be a self-contained bookmarkelet, including jquery is a little trickier than usual.
 (function(){
 
-  function initDOMBookmarklet() {
-    (window.DOMBookmarklet = function() {
-
-      function get3DPageObjects() {
-        var objects = [];
-
-        var recursivelyAddElementToObjects = function(e, zlevel) {
-          var offset = e.offset();
-          objects.push({
-            x: offset.left, 
-            y: offset.top, 
-            z: 10+(10*zlevel), 
-            width: e.outerWidth(), 
-            height: e.outerHeight(), 
-            depth: 10,
-          });
-          e.children().each(function() {
-            recursivelyAddElementToObjects($(this), zlevel + 1);
-          });
-        };
-
-        recursivelyAddElementToObjects($('body'), 1);
-        return objects;
+  function include(url, callback) { 
+    if(url instanceof Array) {
+      if(url.length == 0) {
+        callback();
+        return;
       }
-
-      $(function() {
-        var camera, scene, renderer, controls;
-        var clock = new THREE.Clock();
-
-        init();
-        animate();
-
-        // adds an object with the given dimensions to the scene, handling conversions between coordinate systems.
-        function addBlock(x, y, z, width, height, depth) {
-            var geometry = new THREE.CubeGeometry(width,height,depth);
-            var material = new THREE.MeshBasicMaterial( { color: 0xff00ff*Math.random(), wireframe: false } );
-
-            var mesh = new THREE.Mesh( geometry, material );
-            mesh.position.x = x+(width/2);
-            mesh.position.y = (-1*y)+(height/-2);
-            mesh.position.z = z;
-            scene.add(mesh);
-        }
-
-        function init() {
-          camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-          camera.position.z = 500;
-
-          controls = new THREE.FirstPersonControls(camera);
-          controls.movementSpeed = 1000;
-          controls.lookSpeed = 0.1;
-          scene = new THREE.Scene();
-
-          var objects = get3DPageObjects();
-          for(var i in objects) {
-            var o = objects[i];
-            console.log(o);
-            addBlock(o.x, o.y, o.z, o.width, o.height, o.depth);
-          }
-
-          renderer = new THREE.WebGLRenderer();
-          renderer.setSize( window.innerWidth, window.innerHeight );
-
-          document.body.appendChild( renderer.domElement );
-          $(renderer.domElement).css('position','fixed').css('left','0').css('top','0').css('background-color','#555');
-        }
-
-        function animate() {
-          // note: three.js includes requestAnimationFrame shim
-          requestAnimationFrame(animate);
-
-          controls.update(clock.getDelta());
-
-          renderer.render(scene, camera);
-        }
-
-
+      include(url[0], function() {
+        include(url.slice(1), callback);
       });
-
-    })();
-  }
-
-
-
-  function initIncludes() {
-    (window.IncludesInit = function() {
-
+      return;
+    }
     var done = false;
     var script = document.createElement("script");
-    script.src = "includes.js";
+    script.src = url;
     script.onload = script.onreadystatechange = function(){
       if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
         done = true;
-        initDOMBookmarklet();
+        callback();
       }
     };
     document.getElementsByTagName("head")[0].appendChild(script);
-
-    })();
   }
 
-  // the minimum version of jQuery we want
-  var v = "1.8.2";
+  include(['jquery-1.8.2.min.js','three.min.js','html2canvas.js'], function() {
+    function get3DPageObjects() {
+      var objects = [];
+      function recursivelyAddElementToObjects(e, zlevel) {
+        var offset = e.offset();
+        objects.push({
+          x: offset.left, 
+          y: offset.top, 
+          z: 10+(10*zlevel), 
+          width: e.outerWidth(), 
+          height: e.outerHeight(), 
+          depth: 10,
+        });
+        e.children().each(function() {
+          recursivelyAddElementToObjects($(this), zlevel + 1);
+        });
+      };
 
-  // check prior inclusion and version
-  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-    var done = false;
-    var script = document.createElement("script");
-    script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-    script.onload = script.onreadystatechange = function(){
-      if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-        done = true;
+      recursivelyAddElementToObjects($('body'), 1);
+      return objects;
+    }
 
-        initIncludes();
+    $(function() {
+      var camera, scene, renderer, controls;
+      var clock = new THREE.Clock();
+
+      // adds an object with the given dimensions to the scene, handling conversions between coordinate systems.
+      function addBlock(x, y, z, width, height, depth) {
+          var geometry = new THREE.CubeGeometry(width,height,depth);
+          var material = new THREE.MeshBasicMaterial( { color: 0xff00ff*Math.random(), wireframe: false } );
+
+          var mesh = new THREE.Mesh( geometry, material );
+          mesh.position.x = x+(width/2);
+          mesh.position.y = (-1*y)+(height/-2);
+          mesh.position.z = z;
+          scene.add(mesh);
       }
-    };
-    document.getElementsByTagName("head")[0].appendChild(script);
-  } else {
-    initIncludes();
-  }
 
-})();
+      function threejs_init() {
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+        camera.position.z = 500;
+
+        controls = new THREE.FirstPersonControls(camera);
+        controls.movementSpeed = 1000;
+        controls.lookSpeed = 0.1;
+        scene = new THREE.Scene();
+
+        var objects = get3DPageObjects();
+        for(var i in objects) {
+          var o = objects[i];
+          addBlock(o.x, o.y, o.z, o.width, o.height, o.depth);
+        }
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+        document.body.appendChild( renderer.domElement );
+        $(renderer.domElement).css('position','fixed').css('left','0').css('top','0').css('background-color','#555');
+      }
+
+      function threejs_animate() {
+        // note: three.js includes requestAnimationFrame shim
+        requestAnimationFrame(threejs_animate);
+        controls.update(clock.getDelta());
+        renderer.render(scene, camera);
+      }
+
+      threejs_init();
+      threejs_animate();
+    }); // jquery dom ready
+
+  }); // includes
+})(); // bookmarklet wrapper
