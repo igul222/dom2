658851ee4b2f266395d77da849f56dd5b7755790
Eugene Lee <eugene.lee337@gmail.com>
2012-10-31 01:36:21 -0400

    added a crapload of js packages and files strewn haphazardly. Don't push this branch as is to the master. The main file I'm working on is the collisions.html in the main folder

--- dom.js
+++ dom.js
@@ -1,6 +1,6 @@
 // since this is supposed to be a self-contained bookmarkelet, including jquery is a little trickier than usual.
 (function(){
-
+	
   function include(url, callback) { 
     if(url instanceof Array) {
       if(url.length == 0) {
@@ -24,7 +24,7 @@
     document.getElementsByTagName("head")[0].appendChild(script);
   }
 
-  include(['jquery-1.8.2.min.js','three.min.js','html2canvas.js'], function() {
+  include(['jquery-1.8.2.min.js','three.min.js','html2canvas.js', 'physi.js', 'stats.js'], function() {
     function get3DPageObjects() {
       var objects = [];
       function recursivelyAddElementToObjects(e, zlevel) {
@@ -47,6 +47,8 @@
     }
 
     $(function() {
+		Physijs.scripts.worker = 'physijs_worker.js';
+		Physijs.scripts.ammo = 'ammo.js';
       var camera, scene, renderer, controls;
       var clock = new THREE.Clock();
 
@@ -74,16 +76,73 @@
       }
 
       function threejs_init() {
-        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-        camera.position.z = 500;
-
-        controls = new THREE.FirstPersonControls(camera);
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+		/**/controls = new THREE.FirstPersonControls(camera);
         controls.movementSpeed = 1000;
-        controls.lookSpeed = 0.1;
+        controls.lookSpeed = 0.1;/**/
+		
+        camera.position.z = 300;
+
         scene = new THREE.Scene();
 
-        var objects = get3DPageObjects();
+        //var objects = get3DPageObjects();
 
+		//LOLOL ADDDDEDEDDD STYFFFF__________________________==============================$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+		//document.addEventListener( 'mousemove', onMouseMove, false );
+		
+		// create a sphere shape        
+		geometry = new THREE.SphereGeometry( 16, 16, 16 );
+
+		// give a shape red color
+		material = new THREE.MeshLambertMaterial({color: 0xFF1111});    
+
+		// create an object
+		mesh = new THREE.Mesh( geometry, material );
+
+		mesh.position.x = 100;
+		mesh.position.y = -100;
+		mesh.position.z = 100;
+
+		// add it to the scene
+		scene.add( mesh );
+		
+		// Light
+		light = new THREE.DirectionalLight( 0xFFFFFF );
+		light.position.set( 20, 40, -15 );
+		light.target.position.copy( scene.position );
+		light.castShadow = true;
+		light.shadowCameraLeft = -60;
+		light.shadowCameraTop = -60;
+		light.shadowCameraRight = 60;
+		light.shadowCameraBottom = 60;
+		light.shadowCameraNear = 20;
+		light.shadowCameraFar = 200;
+		light.shadowBias = -.0001
+		light.shadowMapWidth = light.shadowMapHeight = 2048;
+		light.shadowDarkness = .7;
+		scene.add( light );
+		
+		//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+		
+		// Ground
+		ground_material = Physijs.createMaterial(
+			new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture( 'images/rocks.jpg' ) }),
+			.8, // high friction
+			.3 // low restitution
+		);
+		ground_material.map.wrapS = ground_material.map.wrapT = THREE.RepeatWrapping;
+		ground_material.map.repeat.set( 3, 3 );
+		
+		ground = new Physijs.BoxMesh(
+			new THREE.CubeGeometry(100, 100, 1),
+			ground_material,
+			0 // mass
+		);
+		ground.receiveShadow = true;
+		scene.add( ground );
+		
+		spawnBox();
+		
         html2canvas( [ $('body')[0] ], {
           onrendered: function( canvas ) {
             var bodyImg = new Image();
@@ -127,24 +186,139 @@
 
             document.body.appendChild( renderer.domElement );
             $(renderer.domElement).css('position','fixed').css('left','0').css('top','0').css('background-color','#333');
-            threejs_animate();
+            //threejs_animate();
 
           }
         });
+		//setInterval(update,1000/30); 
+		
+		//physijs init stuff
+		/*$('body').append('<div id="viewport"></div>');
+		render_stats = new Stats();
+		render_stats.domElement.style.position = 'absolute';
+		render_stats.domElement.style.top = '0px';
+		render_stats.domElement.style.zIndex = 100;
+		document.getElementById( 'viewport' ).appendChild( render_stats.domElement );
+		
+		physics_stats = new Stats();
+		physics_stats.domElement.style.position = 'absolute';
+		physics_stats.domElement.style.top = '50px';
+		physics_stats.domElement.style.zIndex = 100;
+		document.getElementById( 'viewport' ).appendChild( physics_stats.domElement );*/
+
+		scene = new Physijs.Scene;
+		scene.setGravity(new THREE.Vector3( 0, 0, -30 ));
+		scene.addEventListener(
+			'update',
+			function() {
+				scene.simulate( undefined, 1 );
+				physics_stats.update();
+			}
+		);
       }
+	  
+	  spawnBox = (function() {
+		var box_geometry = new THREE.CubeGeometry( 4, 4, 4 ),
+			handleCollision = function( collided_with, linearVelocity, angularVelocity ) {
+				switch ( ++this.collisions ) {
+					
+					case 1:
+						this.material.color.setHex(0xcc8855);
+						break;
+					
+					case 2:
+						this.material.color.setHex(0xbb9955);
+						break;
+					
+					case 3:
+						this.material.color.setHex(0xaaaa55);
+						break;
+					
+					case 4:
+						this.material.color.setHex(0x99bb55);
+						break;
+					
+					case 5:
+						this.material.color.setHex(0x88cc55);
+						break;
+					
+					case 6:
+						this.material.color.setHex(0x77dd55);
+						break;
+				}
+			},
+			createBox = function() {
+				var box, material;
+				
+				material = Physijs.createMaterial(
+					new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture( 'images/plywood.jpg' ) }),
+					.6, // medium friction
+					.3 // low restitution
+				);
+				material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
+				material.map.repeat.set( .5, .5 );
+				
+				//material = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture( 'images/rocks.jpg' ) });
+				
+				box = new Physijs.BoxMesh(
+					box_geometry,
+					material
+				);
+				box.collisions = 0;
+				
+				box.position.set(
+					Math.random() * 15 - 7.5,
+					25,
+					Math.random() * 15 - 7.5
+				);
+				
+				box.rotation.set(
+					Math.random() * Math.PI,
+					Math.random() * Math.PI,
+					Math.random() * Math.PI
+				);
+				
+				box.castShadow = true;
+				box.addEventListener( 'collision', handleCollision );
+				box.addEventListener( 'ready', spawnBox );
+				scene.add( box );
+			};
+		
+		return function() {
+			setTimeout( createBox, 1000 );
+		};
+	})();
+	  
+	  function update() {
+	   /*camera.rotation.y -= ( - mouseX - camera.position.x ) * 0.00005;
+	   camera.rotation.x += ( mouseY - camera.position.y ) * 0.00005;*/
+	   /*camera.position.x += ( - mouseX - camera.position.x ) * 0.05;
+	   camera.position.y += ( mouseY - camera.position.y ) * 0.05;*/
+	   //camera.lookAt( scene.position );
+	   mesh.rotation.y -= 0.005;
+		}
 
       function threejs_animate() {
         // note: three.js includes requestAnimationFrame shim
         requestAnimationFrame(threejs_animate);
         controls.update(clock.getDelta());
         renderer.render(scene, camera);
+		// update stats
+		//render_stats.update();
       }
+	  
+	  function onMouseMove( event ) {
+		  mouseX = event.clientX - halfWidth;
+		  mouseY = event.clientY - halfHeight;
+		}
 
       // if body's background color is transparent, change it to white
       var rgb = $('body').css('background-color');
       rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
       if(rgb[4] === '0')
         $('body').css('background-color','#fff');
+		
+		var halfWidth = window.innerWidth/2, halfHeight = window.innerHeight/2;
 
       threejs_init();
     }); // jquery dom ready
